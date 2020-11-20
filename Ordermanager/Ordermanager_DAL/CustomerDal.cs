12f0779ba0c9deaa8a63c44832c5faef9222ba92
Piet2001 <piet2001@fireman.net>
2020-11-20 10:14:54 +0100

    add connection strings to all DAL's

--- Ordermanager/Ordermanager_DAL/CustomerDal.cs
+++ Ordermanager/Ordermanager_DAL/CustomerDal.cs
@@ -8,6 +8,8 @@ namespace Ordermanager_DAL
 {
     class CustomerDal : ICustomerProvider
     {
+        private readonly string connectionString = "Server=127.0.0.1;Database=ordermanager;Uid=root;Pwd=;";
+
         public List<Customer> GetAllCustomers()
         {
             throw new NotImplementedException();
--- Ordermanager/Ordermanager_DAL/ProductDAL.cs
+++ Ordermanager/Ordermanager_DAL/ProductDAL.cs
@@ -8,6 +8,8 @@ namespace Ordermanager_DAL
 {
     class ProductDAL : IProductProvider
     {
+        private readonly string connectionString = "Server=127.0.0.1;Database=ordermanager;Uid=root;Pwd=;";
+
         public List<Product> GetAllProducts()
         {
             throw new NotImplementedException();
